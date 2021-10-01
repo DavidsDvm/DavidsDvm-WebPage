91c6cdc5930b3fe5d4f7a60177640873dd86b3aa
DavidsDvm <davids10.dvm@gmail.com>
2021-10-01 17:18:17 -0500

    main content component that has all content of the page

---
+++ src/components/MainContent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mainContentContainer">
+    <HomePage />
+    <AboutMe />
+    <Projects />
+  </div>
+</template>
+
+<script>
+import HomePage from './HomePage'
+import AboutMe from './AboutMe'
+import Projects from './Projects'
+
+export default {
+  name: 'MainContent',
+  components: {
+    HomePage,
+    AboutMe,
+    Projects
+  }
+}
+</script>
+
+<style>
+.mainContentContainer {
+  width: 100%;
+  padding-left: 4.6rem;
+}
+</style>
