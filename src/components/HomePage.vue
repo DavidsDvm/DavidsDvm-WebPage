ab82d0fb24b0437fc702eb234d095946cad0cf3f
DavidsDvm <davids10.dvm@gmail.com>
2021-10-01 00:41:31 -0500

    console main commands created

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -12,9 +12,9 @@
             <div class="console-absoluteElement" ref="consoleAbsoluteElement">
                 <div class="console">
                     <div class="output" v-html="consoleOutput" ref="consoleData"></div>
-                    <div class="action">
+                    <div class="action" ref="consoleInputFocus">
                         <span>dev@DavidsDvm$:  </span>
-                        <textarea class="input" name="input" cols="30" rows="1" @keydown.enter="submitConsole($event)" ref="consoleInputFocus"></textarea>
+                        <textarea class="input" name="input" cols="30" rows="1" @keydown.enter="submitConsole($event)" ref="consoleInput"></textarea>
                     </div>
                 </div>
             </div>
@@ -32,7 +32,8 @@ export default {
         return {
             consoleValue: "",
             consoleChanges: this.$refs.consoleData,
-            avaliableCommands: ['help', 'bison', 'camel', 'duck', 'bison']
+            seperator : '<span class="seperator">== == == == == == == == == == == == == == == == == ==</span><br />',
+            avaliableCommands: ['contact', 'help', 'clear', 'pong', 'about', 'time', 'say {}']
         }   
     },
     mounted() {
@@ -42,57 +43,49 @@ export default {
         submitConsole(event){
             event.preventDefault();
             const avaliableCommandsIndex = this.avaliableCommands.indexOf(event.target.value.trim());
-            if (avaliableCommandsIndex >= 0) {
+            if (event.target.value.trim().startsWith("say")){
+                this.consoleOutputSAY(event.target.value.trim());
                 event.target.value = '';
-			    this.consoleValue = "about"
+            } else if (avaliableCommandsIndex >= 0) {
+                event.target.value = '';
+			    this.consoleValue = this.avaliableCommands[avaliableCommandsIndex];
+            } else {
+                event.target.value = '';
+                this.consoleOutputFALSE();
             }
-            // } else if (inputVal == "ping") {
-            //     pong();
-            //     input.val('');
-            // } else if (inputVal == "about") {
-            //     aboutMe();
-            //     input.val('');
-            // } else if (inputVal == "contact") {
-            //     contactMe();
-            //     input.val('');
-            // } else if (inputVal == "clear") {
-            //     clearConsole();
-            //     input.val('');
-            // } else if (inputVal.startsWith("say") === true) {
-            //     sayThis(inputVal);
-            //     input.val('');
-            // } else if (inputVal.startsWith("sudo") === true) {
-            //     sudo(inputVal);
-            //     input.val('');
-            // } else if (inputVal == "time") {
-            //     getTime();
-            //     input.val('');
-            // } else if (inputVal == 'whats that sound' || inputVal == 'what\'s that sound' || inputVal == 'whats that sound?') {
-            //     seperator();
-            //     Output('<span class="blue">' + inputVal + '</span></br><span class="red">Machine Broken!</span></br>');
-            //     seperator();
-            //     input.val('');
-            // } else if (inputVal.startsWith("exit") === true) {
-            //     Output('<span class="blue">Goodbye! Comeback soon.</span>');
-            //     setTimeout(function() {
-            //         window.open('https://codepen.io/MarioDesigns');
-            //     }, 1000);
-            // } else {
-            //     Output('<span>command not found</span></br>');
-            //     input.val('');
-            // }
         },
         consoleFocus() {
-            const divElement = this.$refs.consoleAbsoluteElement;
+            const divElement = this.$refs.consoleAbsoluteElement
+            this.$refs.consoleInput.focus();
             divElement.scrollTop = divElement.scrollHeight;
-            this.$refs.consoleInputFocus.focus();
             console.log(divElement.scrollTop);
+        },
+        consoleOutputSAY(data) {
+            var allText = this.$refs.consoleData.innerHTML;
+
+            data = data.substr(data.indexOf(' ') + 1);
+	        allText = allText + '<span class="green">[Console]:</span><span>' + data + '</span></br>';
+
+            this.consoleValue = ""
+            allText = allText + this.seperator;
+            this.$refs.consoleData.innerHTML = allText;
+        },
+        consoleOutputFALSE (){
+            var allText = this.$refs.consoleData.innerHTML;
+
+	        allText = allText + '<span class="red">That command doesnt exist!!</span></br>';
+
+            this.consoleValue = ""
+            allText = allText + this.seperator;
+            console.log('sise hizo');
+            this.$refs.consoleData.innerHTML = allText;
         }
     },
     computed: {
         consoleOutput : function() {
-            if (this.consoleValue == "") {
-                return `<span>Initializing...</span><br/>
+            switch (this.consoleValue) {
+                case "":
+                    return `<span>Initializing...</span><br/>
                     <span class="green">0.0002ms ok!</span><br/>
                     <span class="seperator">== == == == == == == == == == == == == == == == == ==</span><br>
                     <pre contenteditable="false"> _______                     __       __          _______                         
@@ -107,25 +100,97 @@ $$$$$$$/  $$$$$$$/    $/    $$/ $$$$$$$/$$$$$$$/ $$$$$$$/     $/    $$/  $$/  $$
                     <span class="seperator">== == == == == == == == == == == == == == == == == ==</span><br>
                     <span>Hope you have fun discovering all the <span class="red">hidden gems</span>!</span>
                     <br>
-                    <span class="blue">type '<span class="grey">help</span>' to see a list of comands available <br/> or '<span class="grey">contact</span>' for a list of ways to contact me.</span><br>`
-            } else if (this.consoleValue == "about"){
-                var contactArray = ['>Contact:', '[GitHub](https://github.com/DavidsDvm)', '[LinkedIn](https://www.linkedin.com/in/davidsdvm/)', '[StackOverflow](https://stackoverflow.com/users/16787643/davidsdvm)', '[Twitter](https://twitter.com/DavidsDvm)'];
-                var allText = this.$refs.consoleData.innerHTML;
-                // seperator();
-                for (var i = 0; i < contactArray.length; i++) {
-                    var out = '<span>' + contactArray[i] + '</span><br/>'
-                    var allText = allText + out;
-                }
-                this.consoleValue = ""
-                this.consoleFocus();
-                return (allText);
-                // seperator();
+                    <span class="blue">type '<span class="grey">help</span>' to see a list of comands available <br/> or '<span class="grey">contact</span>' for a list of ways to contact me.</span><br />
+                    <span class="seperator">== == == == == == == == == == == == == == == == == ==</span><br>`
+                
+                case 'clear':
+                    return ``
+
+                case 'help':
+                    var commandsArray = ['Help: List of available commands',
+                                         '>help',
+                                         '>about',
+                                         '>contact',
+                                         '>ping',
+                                         '>time',
+                                         '>clear',
+                                         '>say'];
+                    var allText = this.$refs.consoleData.innerHTML;
+
+                    for (var i = 0; i < commandsArray.length; i++) {
+                        var out = '<span>' + commandsArray[i] + '</span><br/>'
+                        var allText = allText + out;
+                    }
+
+                    this.consoleValue = ""
+                    allText = allText + this.seperator;
+
+                    return (allText);
+
+                case 'contact':
+                    var contactArray = ['>Contact:', '[GitHub](<span class="blue">https://github.com/DavidsDvm</span>)',
+                                        '[LinkedIn](<span class="blue">https://www.linkedin.com/in/davidsdvm/)</span>',
+                                        '[StackOverflow](<span class="blue">https://stackoverflow.com/users/16787643/davidsdvm)</span>',
+                                        '[Twitter](<span class="blue">https://twitter.com/DavidsDvm)</span>'];
+                    var allText = this.$refs.consoleData.innerHTML;
+
+                    for (var i = 0; i < contactArray.length; i++) {
+                        var out = '<span>' + contactArray[i] + '</span><br/>'
+                        var allText = allText + out;
+                    }
+
+                    this.consoleValue = ""
+                    allText = allText + this.seperator;
+
+                    return (allText);
+                
+                case 'pong':
+                    var allText = this.$refs.consoleData.innerHTML;
+                    var allText = allText + '<span>pong</span></br><span class="pong"><b class="left">|</b><b class="right">|</b></span></br>';
+                    allText = allText + this.seperator;
+                    return (allText)
+
+                case 'about':
+                    var aboutMeArray = ['>About:',
+                                        'Hi There!',
+                                        'I\'m David, better known as \'DavidsDvm\' I am more than a robot coding 🤖, I am a real human trying to learn new things and develop incredible projects with ❤️ meeting incredible people on the wayrocket',
+                                        'Feel free to follow me on GitHub @DavidsDvm - see contacts page.'];
+                    var allText = this.$refs.consoleData.innerHTML;
+
+                    for (var i = 0; i < aboutMeArray.length; i++) {
+                        var out = '<span>' + aboutMeArray[i] + '</span><br/>'
+                        var allText = allText + out;
+                    }
+
+                    this.consoleValue = ""
+                    allText = allText + this.seperator;
+
+                    return (allText);
+
+                case 'time':
+                    var currentdate = new Date(); 
+                    var datetime = "Actual time: " + currentdate.getDate() + "/"
+                                    + (currentdate.getMonth()+1)  + "/" 
+                                    + currentdate.getFullYear() + " @ "  
+                                    + currentdate.getHours() + ":"  
+                                    + currentdate.getMinutes() + ":" 
+                                    + currentdate.getSeconds() + '<br>';
+
+                    var allText = this.$refs.consoleData.innerHTML;
+                    var allText = allText + datetime;
+                    this.consoleValue = ""
+                    allText = allText + this.seperator;
+
+                    return (allText)
+
+                default:
+                    break;
             }
         }
     },
     watch: {
-        consoleChanges : function () {
-            console.log('hola');
+        consoleOutput : function () {
+            this.consoleFocus();
         }
     }
 }
@@ -302,20 +367,17 @@ $$$$$$$/  $$$$$$$/    $/    $$/ $$$$$$$/$$$$$$$/ $$$$$$$/     $/    $$/  $$/  $$
 .console-absoluteElement {
     position: relative;
     overflow: hidden;
-    width: 100%;
-	height: 100%;
+    max-width: 100%;
+	max-height: 100%;
+    display: flex;
+    flex-direction: column-reverse;
 }
 
 .console {
-	position: absolute;
 	padding: 10px;
     box-sizing: border-box;
-    bottom: 0;
-    left: 0;
-    right: 0;
 }
 .console .output {
-    width: 100%;
     font-size: 12px;
     color: #cccccc;
 }
@@ -396,18 +458,20 @@ span.seperator {
     animation: ball 6s linear infinite;
 }
 
-b {
+.pong b {
     display: inline-block;
     position: absolute;
     top: 50%;
     margin-top: -7px;
     transform: translateY(0);
 }
-.b .left {
+
+.pong .left {
     left: -10px;
     animation: raketes 5s ease-in-out infinite;
 }
-.b .right {
+
+.pong .right {
     right: -10px;
     animation: raketes 5s ease-in-out 0.5s infinite;
 }
