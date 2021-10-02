74b1a3b44d8da5ea10bf0188499ba95953918682
DavidsDvm <davids10.dvm@gmail.com>
2021-10-02 13:32:08 -0500

    componenet who check if the user is  viewing that site

---
+++ src/components/ScrollWatcher.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :ref="sentinalName" class="w-full h-px relative" />
+</template>
+
+<script>
+export default {
+    name: 'IntersectionObserver',
+    props: {
+        sentinalName: {
+            type: String,
+            required: true,
+        },
+    },
+    data(){
+        return {
+            isIntersectingElement: false,
+        }
+    },
+    watch: {
+        isIntersectingElement: function(value) {
+            if (value == true) {
+                this.$emit('on-intersection-element', value)
+            }
+        },
+    },
+    mounted() {
+        const sentinal = this.$refs[this.sentinalName]
+        const handler = (entries) => {
+            if (entries[0].isIntersecting) {
+                this.isIntersectingElement = true
+            } 
+            else {
+                this.isIntersectingElement = false
+            }
+        }
+        const observer = new window.IntersectionObserver(handler)
+        observer.observe(sentinal)
+    },
+}
+</script>
